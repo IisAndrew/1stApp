aa33acc0e25dc6103c08018380f34be9653cd4d5
202732R@mymail.nyp.edu.sg <202732R@mymail.nyp.edu.sg>
2021-12-13 14:57:25 +0800

    check for merge problems

--- app/src/main/java/com/example/a1stapp/SplashPage.java
+++ app/src/main/java/com/example/a1stapp/SplashPage.java
@@ -1,7 +1,6 @@
 package com.example.a1stapp;
 
 import android.app.Activity;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
